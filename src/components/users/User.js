4eda1aac211eabfdc7419aa7ded70edfccb6c430
Quazi Rahmatullah <shaiun@outlook.com>
2021-07-05 00:25:57 +1000

    Lecture 26 -  Search Class to Function With useState Hook

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,6 +1,5 @@
 import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
-import Spinner from '../layout/Spinner';
 import PropTypes from 'prop-types';
 import Repos from '../repos/Repos';
 
