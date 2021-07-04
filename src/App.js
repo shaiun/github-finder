c5a9ba47283c7119c90208fad65a1101ee82bfef
Quazi Rahmatullah <shaiun@outlook.com>
2021-07-04 13:48:53 +1000

    Lecture 17 - Events & Search Component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,40 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
+
 import axios from 'axios';
 
 class App extends Component {
-
   state = {
     users: [],
-    loading : false
-  }
+    loading: false,
+  };
 
-   async componentDidMount(){
-    this.setState({loading : true});
+  async componentDidMount() {
+    this.setState({ loading: true });
     const res = await axios.get(`https://api.github.com/users?client_id=
     ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
     ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    
+
     this.setState({
-        users: res.data, 
-        loading : false
-      });
+      users: res.data,
+      loading: false,
+    });
   }
 
-  render(){
+  render() {
     return (
-      <div className="App">
-        <Navbar title='Github Finder'/>
-        <div className="container">
-          <Users loading={this.state.loading} users={this.state.users}/>
+      <div className='App'>
+        <Navbar title='Github Finder' />
+        <div className='container'>
+          <Search />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
   }
 }
-
 
 export default App;
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,33 +1,31 @@
 import React from 'react';
 import UserItem from './UserItem';
-import Spinner from '../layout/Spinner'
-import PropTypes from 'prop-types'
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
 
-const Users =({users, loading})=> {
-    if(loading){
-        return <Spinner/>
-    }else{
-        return (
-        <div style={userStyle}>
-            {users.map(user=>(
-                <UserItem key={user.id} user={user}/>
-            ))}
-        </div>
-    )
-}
-
-}
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
+    return (
+      <div style={userStyle}>
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  }
+};
 
 const userStyle = {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(3, 1fr)',
-    gridGap: '1rem'
-  };
+  display: 'grid',
+  gridTemplateColumns: 'repeat(3, 1fr)',
+  gridGap: '1rem',
+};
 
-  Users.prototype ={
-    users : PropTypes.array.isRequired,
-    loading : PropTypes.bool.isRequired,
-}
+Users.prototype = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
 
-
-export default Users
+export default Users;
