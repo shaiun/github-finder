f174e02744852f1e3fc5489296e607cb109a7093
Quazi Rahmatullah <shaiun@outlook.com>
2021-07-04 23:39:16 +1000

    Lecture 24 - Repos & RepoItem Component & Data

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ class App extends Component {
     user: {},
     loading: false,
     alert: null,
+    repos: [],
   };
 
   // Search Github users through props from Search.js
@@ -43,6 +44,19 @@ class App extends Component {
     });
   };
 
+  getUserRepos = async (username) => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc
+      &client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+      &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({
+      repos: res.data,
+      loading: false,
+    });
+  };
+
   clearUsers = () => {
     this.setState({ users: [], loading: false });
   };
@@ -53,7 +67,7 @@ class App extends Component {
   };
 
   render() {
-    const { users, loading, user } = this.state;
+    const { users, loading, user, repos } = this.state;
 
     return (
       <Router>
@@ -85,7 +99,9 @@ class App extends Component {
                   <User
                     {...props}
                     getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
                     user={user}
+                    repos={repos}
                     loading={loading}
                   />
                 )}
